ddcee371190da46cd1fa2bde3d60e988d89407fe
caminadab <caminadab@7b5ee9f1-2805-22ce-3cb8-aa63eb576c5b>
2010-11-20 00:23:05 +0000

    MUX do PC, PC+4 e Pipeline do PC

--- mips.vhd
+++ mips.vhd
@@ -22,7 +22,37 @@ component sum32
 		  Result: out std_logic_vector(X-1 downto 0);
 		  Overflow,CarryOut: out std_logic);
 	end component;
-	signal PCPlus4F : std_logic_vector(X-1 downto 0);
+	
+	
+	--Guarda o valor do PC atual + 4, do PC de Branch e do PC atual
+	signal PCPlus4F, PCBranchM, PC : std_logic_vector(X-1 downto 0);
+	--Signal auxiliar que contem o mesmo valor que a saida PCF
+	signal PCFaux : std_logic_vector(X-1 downto 0);
+	--Seletor do MUX do PC
+	signal PCsrcM : std_logic; 
+	
+	
 Begin
-	PCPlus4Fsum: sum32 PORT MAP(a=>PCF,b=>'00000000000000000000000000000100',CarryIn=>'0',Result=>PCPlus4F);
+	PCPlus4Fsum: sum32 PORT MAP(a=>PCFaux,b=>'00000000000000000000000000000100',CarryIn=>'0',Result=>PCPlus4F);
+	
+	PCmux : Process (PCPlus4F, PCsrcM, PCBranchM)
+	begin
+		Case PCsrcM is
+			When '0' =>
+				PC <= PCPlus4F;
+			When '1' =>
+				PC <= PCBranchM;
+			When others =>
+				null;
+		end Case;
+	end PCmux;
+	
+	PCPipeline : Process (clk)
+	begin
+		if clk 'EVENT AND clk = '1' then
+			PCF <= PC;
+			PCFaux <= PC;
+		end if; 
+	end PCPipeline;
+	
 End behaviour;
